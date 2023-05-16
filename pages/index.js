0776b138dc85a13fa006f9247d32e2822b6694eb
Cristian Webber <cristian.webber@hotmail.com>
2023-05-16 10:56:40 +0200

    feat: make lottery works

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import { Inter } from 'next/font/google'
-import Header from "../components/Header";
+import Header from "@/components/Header";
+import Lottery from '@/components/Lottery';
+import { useMoralis } from "react-moralis";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
+  const { isWeb3Enabled, chainId } = useMoralis();
+
   return (
     <div>
-      <Header></Header>
-      Hello
+      <Header/>
+      <Lottery/>
     </div>
   )
 }
